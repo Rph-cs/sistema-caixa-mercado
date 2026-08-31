4d2334e0688bf8fdf583ba56324efa6238487c51
Davialexander0310 <davialexandercunhabarbosa@gmail.com>
2026-08-31 00:50:46 -0300

    feat: histórico de vendas com carregamento automático e pesquisa

--- src/View/Tela_Inicial_GUI.java
+++ src/View/Tela_Inicial_GUI.java
@@ -7,6 +7,12 @@ public class Tela_Inicial_GUI extends javax.swing.JFrame {
         jTable1.getColumnModel().getColumn(4).setMinWidth(0);
         jTable1.getColumnModel().getColumn(4).setMaxWidth(0);
         jTable1.getColumnModel().getColumn(4).setWidth(0);
+
+        jTabbedPane1.addChangeListener(e -> {
+            if (jTabbedPane1.getSelectedIndex() == 1) {
+                carregarHistorico();
+            }
+        });
     }
 
     @SuppressWarnings("unchecked")
@@ -31,6 +37,11 @@ public class Tela_Inicial_GUI extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         btnFinalizar = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        btnPesquisar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -151,7 +162,56 @@ public class Tela_Inicial_GUI extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Caixa", new javax.swing.ImageIcon(getClass().getResource("/Image/caixa.png")), Caixa, ""); // NOI18N
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(null);
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
+            },
+            new String [] {
+                "Nº da venda", "Data/hora", "Total", "Forma de pagamento", "Status"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
+
+        jPanel1.add(jScrollPane2);
+        jScrollPane2.setBounds(40, 80, 770, 320);
+
+        jLabel3.setFont(new java.awt.Font("Arial", 0, 20)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Informe a compra:");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(40, 30, 170, 40);
+
+        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField4ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jTextField4);
+        jTextField4.setBounds(220, 30, 420, 40);
+
+        btnPesquisar.setBackground(new java.awt.Color(0, 0, 255));
+        btnPesquisar.setForeground(new java.awt.Color(255, 255, 255));
+        btnPesquisar.setText("Pesquisar");
+        btnPesquisar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPesquisarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnPesquisar);
+        btnPesquisar.setBounds(660, 30, 150, 40);
+
         jTabbedPane1.addTab("Historico", new javax.swing.ImageIcon(getClass().getResource("/Image/historia.png")), jPanel1); // NOI18N
 
         jTabbedPane1.setSelectedComponent(Caixa);
@@ -194,8 +254,7 @@ public class Tela_Inicial_GUI extends javax.swing.JFrame {
             modelo.addRow(new Object[]{produto.getNome(), quantidade, produto.getPreco(), subtotal, produto.getIdProduto()});
             jTextField1.setText("");
             jTextField3.setText("");
-        } 
-        else {
+        } else {
             javax.swing.JOptionPane.showMessageDialog(null, "Produto não encontrado!");
         }
     }//GEN-LAST:event_btnAdicionarActionPerformed
@@ -205,8 +264,7 @@ public class Tela_Inicial_GUI extends javax.swing.JFrame {
         if (linhaSelecionada >= 0) {
             javax.swing.table.DefaultTableModel modelo = (javax.swing.table.DefaultTableModel) jTable1.getModel();
             modelo.removeRow(linhaSelecionada);
-        } 
-        else {
+        } else {
             javax.swing.JOptionPane.showMessageDialog(null, "Selecione um item para deletar!");
         }
     }//GEN-LAST:event_btnDeletarActionPerformed
@@ -250,6 +308,48 @@ public class Tela_Inicial_GUI extends javax.swing.JFrame {
         this.setVisible(false);
     }//GEN-LAST:event_btnFinalizarActionPerformed
 
+    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+
+    }//GEN-LAST:event_jTextField4ActionPerformed
+
+    private void carregarHistorico() {
+        String filtro = jTextField4.getText().trim();
+        javax.swing.table.DefaultTableModel modelo = (javax.swing.table.DefaultTableModel) jTable2.getModel();
+        modelo.setRowCount(0);
+
+        String sql = filtro.isEmpty()
+                ? "SELECT id_venda, data_hora, total, forma_pagamento, status FROM VENDAS ORDER BY data_hora DESC"
+                : "SELECT id_venda, data_hora, total, forma_pagamento, status FROM VENDAS WHERE id_venda = ? ORDER BY data_hora DESC";
+
+        try {
+            java.sql.Connection conn = config.ConexaoBD.conectar();
+            java.sql.PreparedStatement stmt = conn.prepareStatement(sql);
+            if (!filtro.isEmpty()) {
+                stmt.setString(1, filtro);
+            }
+            java.sql.ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                modelo.addRow(new Object[]{
+                    rs.getInt("id_venda"),
+                    rs.getString("data_hora"),
+                    rs.getDouble("total"),
+                    rs.getString("forma_pagamento"),
+                    rs.getString("status")
+                });
+            }
+        } 
+        
+        catch (java.sql.SQLException e) {
+            javax.swing.JOptionPane.showMessageDialog(null, "Erro ao carregar histórico: " + e.getMessage());
+        }
+    }
+
+
+    private void btnPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarActionPerformed
+        carregarHistorico();
+    }//GEN-LAST:event_btnPesquisarActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -268,17 +368,11 @@ public class Tela_Inicial_GUI extends javax.swing.JFrame {
             }
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(Tela_Inicial_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } 
-        
-        catch (InstantiationException ex) {
+        } catch (InstantiationException ex) {
             java.util.logging.Logger.getLogger(Tela_Inicial_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } 
-        
-        catch (IllegalAccessException ex) {
+        } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(Tela_Inicial_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } 
-        
-        catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Tela_Inicial_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
@@ -297,18 +391,23 @@ public class Tela_Inicial_GUI extends javax.swing.JFrame {
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnDeletar;
     private javax.swing.JButton btnFinalizar;
+    private javax.swing.JButton btnPesquisar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
